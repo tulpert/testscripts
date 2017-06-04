848d70629c22f8ab634c7ed7611877cb2b719b7a
Tulpert <tulpert@outlook.com>
2017-06-04 12:18:49 +0200

    Added an example to generate write-process status bars

---
+++ Write-Process_example.ps1
@@ -0,0 +1,26 @@
+﻿ $a = @{}; 
+ $sleeptime = 15
+ 
+ 1,2,3 | ForEach-Object { 
+    $b = Start-Process -PassThru -Verbose  sleep $sleeptime
+    $a.add($_, $b) 
+ }
+
+
+while ($a.Keys.Count -gt 0) {
+    $remove = @()
+    
+    $a.Keys | ForEach-Object {
+        $_proc = Get-Process -id $a[$_].Id -ErrorAction SilentlyContinue
+        if ( $_proc ) { 
+            $sec = ((Get-Date) - $_proc.starttime).Seconds
+            # "ProcessID [" + $_proc.id + "] has been running for " + $sec + " seconds"
+            Write-Progress -Activity ("Process "+ $_proc.id)  -Id $_proc.id -SecondsRemaining ($sleeptime - $sec) -PercentComplete ($sec / $sleeptime * 100)
+        } else { $remove += $_}
+    }
+
+    $remove | ForEach-Object {
+        $a.Remove($_)
+    }
+
+}
